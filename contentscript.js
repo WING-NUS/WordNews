36b386456125f60164d6827c0cd0ad8afc766746
laizhouxia <znj472982642@gmail.com>
2014-09-01 14:25:13 +0800

    first commit.

---
+++ contentscript.js
@@ -0,0 +1,216 @@
+'use script';
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+var userAccount;
+var isWorking;
+
+function occurrences(string, substring) {
+	var n = 0;
+	var pos = 0;
+	var l = substring.length;
+
+	while (true) {
+		pos = string.indexOf(substring, pos);
+		if (pos > -1) {
+			n++;
+			pos += l;
+		} else {
+			break;
+		}
+	}
+	return (n);
+}
+
+
+function talkToHeroku(url, params, index){
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    console.log("here");
+    xhr.onreadystatechange = function() {//Call a function when the state changes.
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            console.log("here?");
+            var response = xhr.responseText.replace(/&quot;/g,'"');
+            var obj=JSON.parse(response);
+            console.log(obj);
+            //document.getElementByTagName("title").innerHTML = obj["chinese"]
+            if(obj.chinese!==undefined){
+                //console.log(obj.chinese);
+            }
+            var data = JSON.parse(response);
+            var sourceWords = [];
+            var targetWords = [];
+            for (var x in data) {
+				sourceWords.push(x);
+				targetWords.push(data[x]);
+				console.log(x+" "+data[x]);
+			}
+			replaceWords(sourceWords, targetWords, index);
+            //document.getElementById('article').innerHTML  = obj["chinese"];
+        }
+        else {// Show what went wrong
+            //document.getElementById('article').innerHTML  = "Something Went Wrong";
+        }
+    }
+    xhr.send(params);
+}
+
+
+function replaceWords(sourceWords, targetWords, i){
+
+	var paragraphs = document.getElementsByClassName('cnn_storypgraphtxt');
+
+	for(var j = 0;j < sourceWords.length; j++){
+
+		var sourceWord = sourceWords[j];
+		var targetWord = targetWords[j];
+
+    	var paragraph = paragraphs[i];
+    	var text = paragraph.innerHTML;
+    	if(i==0){
+    		console.log(paragraph.innerHTML);
+    		console.log(paragraph.innerText);
+    	}
+
+		var id = "myID_"+sourceWord+"_"+i.toString();
+		console.log(id);
+
+		var popoverContent = "";
+    	popoverContent += "<button id=\""+ id + "_btn1\" class=\"btn btn-info\">Got it</button>";
+    	popoverContent += "<span>    </span>"
+    	popoverContent += "<button id=\""+ id + "_btn2\" class=\"btn btn-warning\">Show me</button>";
+
+		var joinString = "";
+		joinString += "  <span ";
+		joinString += "data-placement='above' ";
+		joinString += "title='"+ "Translated from: <span style=\"font-weight: bold;  font-size:150%;\">" + sourceWord + "</span>' ";
+		joinString += "href='#' ";
+		joinString += "data-content = '" + popoverContent + "'";
+		joinString += "";
+		joinString += "";
+		joinString += "id = '" + id + "' >";
+		joinString += targetWord;
+		joinString += "</span>  ";
+		//console.log(joinString);
+
+		$(document).on("click", "#"+id+"_btn1", function() {
+			var id = $(this).attr('id');
+		    var word = id.split('_')[1];
+	    	var remembered = new HttpClient();
+			remembered.get('http://localhost:3000/remember?name='+userAccount+'&word='+word+'&is_remembered=1', function(answer) {
+			    console.log("this is answer: "+answer);
+			});
+			$('button').popover('hide');
+		});
+
+		$(document).on("click", "#"+id+"_btn2", function() {
+			var id = $(this).attr('id');
+		    var word = id.split('_')[1];
+	    	var remembered = new HttpClient();
+	    	$('button').popover('hide');
+			remembered.get('http://localhost:3000/remember?name='+userAccount+'&word='+word+'&is_remembered=0', function(answer) {
+			    console.log("this is answer: "+answer);
+			});
+			window.open("http://dict.youdao.com/search?q="+word+"&keyfrom=dict.index");
+		});
+
+		var parts = text.split(" " + sourceWord + " ");
+		var t = 1;
+
+		for(var k=0; k< parts.length; k++){
+			var n = occurrences(parts[k],"\"");
+			if(n%2==1){
+				if(t == 1){
+					parts[k] = parts[k] + "\"";
+					t = 2;
+				}
+				else{
+					parts[k] = "\"" + parts[k];
+					t = 1;
+				}
+			}
+		}
+
+    	var result = parts.join(joinString);
+
+    	paragraph.innerHTML = result;
+	}
+
+	$('button').popover({ html : true});
+
+	$('button').click(function(){
+  		var currentId = $(this).attr('id');
+  		console.log(currentId);
+  		if(currentId.indexOf('myID_') > -1 && (!currentId.indexOf('_btn') > -1)){
+			$(this).popover({
+				trigger : 'focus',
+				html : true,
+			    content: function() {
+			      return $('#popover_content_wrapper').html();
+			    }
+			});
+  		}
+	});
+}
+
+
+window.addEventListener("load", function(){
+
+	userAccount = localStorage.getItem("userAccount");
+	isWorking = localStorage.getItem("isWorking");
+	if (userAccount == undefined){
+		var d = new Date();
+		userAccount = "id"+d.getTime()+"_1";
+		localStorage.setItem("userAccount",userAccount);
+	}
+	console.log(userAccount);
+	if(isWorking == undefined)
+	{
+		isWorking = 0;
+		localStorage.setItem("isWorking",isWorking);
+	}
+	console.log("isWorking "+isWorking);
+
+	if(isWorking == 1)
+	{
+		var paragraphs = document.getElementsByClassName('cnn_storypgraphtxt');
+
+		for (var i = 0; i < paragraphs.length; i++) {
+
+			var sourceWords = [];
+			var targetWords = [];
+
+			var stringToServer = paragraphs[i];
+			stringToServer = stringToServer.innerHTML;
+
+		    var url='http://testnaijia.herokuapp.com/show';
+		    var params = "text="+stringToServer;
+		    talkToHeroku(url, params, i);
+		}
+	}
+
+});
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+    if (request.action == "toggleTranslate")
+    {
+		localStorage.setItem("isWorking",request.working);
+    }
+    if (request.action == "changeUserID")
+    {
+    	localStorage.setItem("userAccount",request.account);
+    }
+});
---
+++ popup.js
@@ -0,0 +1,100 @@
+	var HttpClient = function() {
+		this.get = function(aUrl, aCallback) {
+				anHttpRequest = new XMLHttpRequest();
+				anHttpRequest.onreadystatechange = function() { 
+						if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+								aCallback(anHttpRequest.responseText);
+				}
+				anHttpRequest.open( "GET", aUrl, true );						
+				anHttpRequest.send( null );
+		}
+	}
+
+
+	function onWindowLoad() {
+
+	var userAccount = localStorage.getItem("userAccount");
+	var isWorking = localStorage.getItem("isWorking");
+	if (userAccount == undefined){
+		var d = new Date();
+		userAccount = "id"+d.getTime()+"_1";
+		localStorage.setItem("userAccount",userAccount);
+	}
+	console.log("userAccount "+userAccount);
+
+	if(isWorking == undefined)
+	{
+		isWorking = 0;
+		localStorage.setItem("isWorking",isWorking);
+	}
+	console.log("isWorking "+isWorking);
+	if(isWorking == 0)
+	{
+		document.getElementById("turnOn").className = "btn btn-default";
+		document.getElementById("turnOff").className = "btn btn-primary active";
+	}
+	else
+	{
+		document.getElementById("turnOn").className = "btn btn-primary active";
+		document.getElementById("turnOff").className = "btn btn-default";
+	}
+	var remembered = new HttpClient();
+	var answer;
+	remembered.get('http://localhost:3000/getNumber?name='+userAccount, function(answer) {
+			console.log("this is answer: "+answer);
+			var obj=JSON.parse(answer);
+			console.log(obj);
+			if(obj.learnt!==undefined)
+			{
+				document.getElementById("learnt").innerHTML = obj["learnt"];
+			}
+			if(obj.tolearn!==undefined)
+			{
+				document.getElementById("toLearn").innerHTML = obj["tolearn"];
+			}
+	});
+
+	$('.btn-toggle').click(function() {
+			//$(this).find('.btn').toggleClass('active');	
+			console.log("isWorking1 "+isWorking);
+			if(isWorking==1)
+			{
+				isWorking = 0;
+				localStorage.setItem("isWorking",0);
+
+			}
+			else
+			{
+				isWorking = 1;
+				localStorage.setItem("isWorking",1);
+			}
+			console.log("isWorking2 "+isWorking);
+
+			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+				chrome.tabs.sendMessage(tabs[0].id, { action: "toggleTranslate" , working: isWorking});
+			});
+
+			$(this).find('.btn').toggleClass('active');	
+			
+			if ($(this).find('.btn-primary').size()>0) {
+				$(this).find('.btn').toggleClass('btn-primary');
+			}
+			if ($(this).find('.btn-danger').size()>0) {
+				$(this).find('.btn').toggleClass('btn-danger');
+			}
+			if ($(this).find('.btn-success').size()>0) {
+				$(this).find('.btn').toggleClass('btn-success');
+			}
+			if ($(this).find('.btn-info').size()>0) {
+				$(this).find('.btn').toggleClass('btn-info');
+			}
+			
+			$(this).find('.btn').toggleClass('btn-default');
+	});
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "changeUserID" , account: userAccount});
+  });
+}
+
+	window.onload = onWindowLoad;
