40c8622cbe8b3daa71a1b8455f0f67fb72d4836a
Zhao Yue <endlessfrank@qq.com>
2014-10-27 00:21:23 +0800

    Fixed some migration files

--- db/migrate/20141026142318_prepare_dict.rb
+++ db/migrate/20141026142318_prepare_dict.rb
@@ -1,15 +1,5 @@
 class PrepareDict < ActiveRecord::Migration
   def change
-    create_table :chinese_words do |t|
-      t.string :chinese_meaning
-      t.timestamps
-    end
- 
-    create_table :english_words do |t|
-      t.string :english_meaning
-      t.timestamps
-    end
- 
     create_table :meanings do |t|
       t.belongs_to :chinese_word
       t.belongs_to :english_word
