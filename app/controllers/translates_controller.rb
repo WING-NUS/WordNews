ed799757522a55dba94d888a4dacceeef951cd82
Kang HJ <kanghongjin@gmail.com>
2015-06-07 10:15:09 +0800

    Omit incorrect replacement

--- app/controllers/translates_controller.rb
+++ app/controllers/translates_controller.rb
@@ -44,45 +44,54 @@ class TranslatesController < ApplicationController
                                         .select('english_meaning, meanings.id, meanings.chinese_words_id')
                                         .where("english_meaning = ?", original_word)
 
-      temp = nil
+      english_meaning = nil
       if english_meaning_row.length == 0
         next
       elsif english_meaning_row.length == 1 #has one meaning
-        temp = english_meaning_row.first
+        english_meaning = english_meaning_row.first
       else
         #### temporarily not translate tokens with multiple possible chinese substitutions ###
         #number_of_meanings.times do |index|
         #  if chinese_sentence.to_s.include? ChineseWords.find(meanings[index].chinese_words_id).chinese_meaning
-        #    temp = meanings[index]
+        #    english_meaning = meanings[index]
         #  end
         #end
         next
+      end
+
+      @text[word] = Hash.new
+
+      # check if a hard-coded translation is specified for this word
+      hard_coded_word = HardCodedWord.where(:url => @url, :word => original_word )
+      if hard_coded_word.length > 0
+        @text[word]['chinese'] = hard_coded_word.first.translation
       end
 
       @original_word_id = english_meaning_row.first.id
       
 
       #if temp.chinese_words_id.nil?
-      #  temp = meanings[0]
+      #  english_meaning = meanings[0]
       #end
 
       # if this point is reached, then the word and related information is sent back
       words_retrieved = words_retrieved + 1
 
-      @text[word] = Hash.new
-      @original_word_chinese_id = temp.chinese_words_id
+      @original_word_chinese_id = english_meaning.chinese_words_id
 
       # see if the user understands this word before
-      @text[word]['wordID'] = temp.id # pass meaningId to client
-      chinese_word = ChineseWords.find(temp.chinese_words_id)
-      @text[word]['chinese'] = chinese_word.chinese_meaning
+      @text[word]['wordID'] = english_meaning.id # pass meaningId to client
+      chinese_word = ChineseWords.find(english_meaning.chinese_words_id)
+      if hard_coded_word.length == 0
+        @text[word]['chinese'] = chinese_word.chinese_meaning
+      end
       @text[word]['pronunciation'] = chinese_word.pronunciation
       
       
       #@user_id = User.where(:user_name => @user_name).first.id
       testEntry = Meaning.joins(:histories)
                          .select('meaning_id, frequency')
-                         .where("user_id = ? AND meaning_id = ?", user_id, temp.id).first
+                         .where("user_id = ? AND meaning_id = ?", user_id, english_meaning.id).first
 
 
       if testEntry.blank? or testEntry.frequency.to_i <= 3  #just translate the word
@@ -98,6 +107,14 @@ class TranslatesController < ApplicationController
           @text[word]['choices'][idx.to_s] = EnglishWords.find(val.english_words_id).english_meaning
         }
 
+        hard_coded_quiz = HardCodedQuiz.where(:url => @url, :word => original_word )
+        if hard_coded_quiz.length > 0
+          
+          @text[word]['choices']['0'] = hard_coded_word.first.option1
+          @text[word]['choices']['1'] = hard_coded_word.first.option2
+          @text[word]['choices']['2'] = hard_coded_word.first.option3
+        end
+
       elsif testEntry.frequency.to_i > 6 and testEntry.frequency.to_i <= 10
         @text[word]['isTest'] = 2
         @text[word]['choices'] = Hash.new
@@ -108,6 +125,13 @@ class TranslatesController < ApplicationController
           @text[word]['choices'][idx.to_s] = ChineseWords.find(val.chinese_words_id).chinese_meaning
         }
 
+        hard_coded_quiz = HardCodedQuiz.where(:url => @url, :word => original_word )
+        if hard_coded_quiz.length > 0
+          
+          @text[word]['choices']['0'] = hard_coded_word.first.option1
+          @text[word]['choices']['1'] = hard_coded_word.first.option2
+          @text[word]['choices']['2'] = hard_coded_word.first.option3
+        end
       elsif testEntry.frequency.to_i >= 11
         @text[word]['isTest'] = 1
         @text[word]['choices'] = Hash.new
@@ -123,6 +147,7 @@ class TranslatesController < ApplicationController
         #  @text[word]['choices'][idx.to_s] = val.strip
         #}
         @text[word]['isChoicesProvided'] = false
+
       end
 
     end # end of for word in word_list
@@ -185,6 +210,14 @@ class TranslatesController < ApplicationController
     distractors.each_with_index { |val, idx|   
       @result[word_under_test]['choices'][idx.to_s] = val.strip
     }
+        
+    hard_coded_quiz = HardCodedQuiz.where(:url => @url, :word => original_word )
+    if hard_coded_quiz.length > 0
+          
+      @text[word]['choices']['0'] = hard_coded_word.first.option1
+      @text[word]['choices']['1'] = hard_coded_word.first.option2
+      @text[word]['choices']['2'] = hard_coded_word.first.option3
+    end
     
     respond_to do |format|
       format.json { render json: @result}
