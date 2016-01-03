2502dc3a2cab1003f0703dd1b2fddfc9f68f4751
Naijia Zheng <Naijia Zheng>
2016-01-03 19:03:59 +0800

    index page partially done, need to refactor and modify UI

--- app/src/main/java/com/example/naijia/wordnews/MainActivity.java
+++ app/src/main/java/com/example/naijia/wordnews/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,8 +14,24 @@ import android.widget.ListView;
 
 import com.example.naijia.wordnews.APIRequest;
 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InterruptedIOException;
+import java.io.StringReader;
 import java.util.concurrent.ExecutionException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
     private PostData[] listData;
@@ -26,13 +43,69 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        try{
+            String url = "http://rss.cnn.com/rss/edition.rss";
+            //String xmlRecords = "<data><terminal_id>1000099999</terminal_id><merchant_id>10004444</merchant_id><merchant_info>Mc Donald's - Abdoun</merchant_info></data>";
+            String response = "";
+            try {
+                response = new APIRequest().execute(url).get();
+            } catch (ExecutionException | InterruptedException e) {
+                //DO something
+            }
 
-        this.generateDummyData();
-        ListView listView = (ListView) this.findViewById(R.id.postListView);
-        PostItemAdapter itemAdapter = new PostItemAdapter(this,
-                R.layout.postitem, listData);
-        listView.setAdapter(itemAdapter);
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new StringReader(response));
+            int eventType = xpp.getEventType();
+            PostData data = null;
+            listData = new PostData[100];
+            int count = 0;
+            data = new PostData();
+            data.postDate = "";
+            data.postTitle = "";
+            data.postThumbUrl = null;
+            String nameTag = "";
+            while (eventType != XmlPullParser.END_DOCUMENT) {
 
+                if (eventType == XmlPullParser.START_DOCUMENT) {
+                    //do something
+                } else if (eventType == XmlPullParser.START_TAG) {
+                    nameTag = xpp.getName();
+                } else if (eventType == XmlPullParser.END_TAG) {
+                    if(xpp.getName().equals("item")) {
+                        Log.d("LOG",data.postTitle);
+                        listData[count] = data;
+                        count++;
+                        if(count>10) break;
+                        data = new PostData();
+                        data.postDate = "";
+                        data.postTitle = "";
+                        data.postThumbUrl = null;
+                    }
+                } else if (eventType == XmlPullParser.TEXT) {
+                    if(nameTag.equals("title")){
+                        data.postTitle = xpp.getText();
+                    } else if (nameTag.equals("pubDate")){
+                        data.postDate = xpp.getText();
+                    }
+                }
+                eventType = xpp.next();
+            }
+            for(int i=0;i<10;i++){
+                Log.d("LOG_FINAL", listData[i].postTitle);
+            }
+            ListView listView = (ListView) this.findViewById(R.id.postListView);
+            PostItemAdapter itemAdapter = new PostItemAdapter(this,
+                    R.layout.postitem, listData);
+            listView.setAdapter(itemAdapter);
+//        } catch (ParserConfigurationException | IOException | SAXException | XmlPullParserException e){
+        } catch (IOException | XmlPullParserException e){
+            this.generateDummyData();
+            ListView listView = (ListView) this.findViewById(R.id.postListView);
+            PostItemAdapter itemAdapter = new PostItemAdapter(this,
+                    R.layout.postitem, listData);
+            listView.setAdapter(itemAdapter);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
