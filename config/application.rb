632d039278f7742b5899589b55909711a0fe08e7
Kang HJ <kanghongjin@gmail.com>
2016-01-21 10:35:39 +0800

    config to include lib

--- config/application.rb
+++ config/application.rb
@@ -56,5 +56,9 @@ module TranslateApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+    # load lib
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
