885b75751b891b99d44aa4988435ee16d2086e87
William Wu <weilun.wu@grabtaxi.com>
2016-01-25 23:26:29 +0800

    Add post request

---
+++ app/src/main/java/com/example/naijia/wordnews/api/PostRequest.java
@@ -0,0 +1,89 @@
+package com.example.naijia.wordnews.api;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by wuweilun on 25/1/16.
+ */
+public class PostRequest extends AsyncTask<String, String, String> {
+    String TAG = "API";
+
+    @Override
+    protected String doInBackground(String... argv) {
+        String responseString = null;
+        try {
+            URL url = new URL(argv[0]);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // add request header
+            conn.setRequestMethod("POST");
+            String urlParameters = argv[1];
+
+            conn.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            if(conn.getResponseCode() == HttpsURLConnection.HTTP_OK){
+                // Do normal input or output stream reading
+                responseString = readInputStreamToString(conn);
+            }
+            else {
+                responseString = "FAILED"; // See documentation for more info on response handling
+            }
+        } catch (IOException e) {
+            //TODO Handle problems..
+        }
+        return responseString;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        //Do anything with response..
+    }
+    private String readInputStreamToString(HttpURLConnection connection) {
+        String result = null;
+        StringBuffer sb = new StringBuffer();
+        InputStream is = null;
+
+        try {
+            is = new BufferedInputStream(connection.getInputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String inputLine = "";
+            while ((inputLine = br.readLine()) != null) {
+                sb.append(inputLine);
+            }
+            result = sb.toString();
+        }
+        catch (Exception e) {
+            Log.i(TAG, "Error reading InputStream");
+            result = null;
+        }
+        finally {
+            if (is != null) {
+                try {
+                    is.close();
+                }
+                catch (IOException e) {
+                    Log.i(TAG, "Error closing InputStream");
+                }
+            }
+        }
+
+        return result;
+    }
+}
