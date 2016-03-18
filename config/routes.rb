56914cb44bd5af7b0b9e14fdc17b703c4a742628
Kang HJ <kanghongjin@gmail.com>
2016-03-18 19:55:50 +0800

    change back

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ TranslateApp::Application.routes.draw do
   
   root to: 'translates#index'
   #match '/articleContents', to: 'translates#paragraphs_in_article', via: :get
-  match '/show', to: 'translates#show', via: :post
+  match '/show', to: 'translates#show_by_bing', via: :post
   match '/showbybing', to: 'translates#show_by_bing', via: :post
   match '/remember', to: 'translates#remember', via: :get
   match '/getQuiz', to: 'translates#quiz', via: :get
